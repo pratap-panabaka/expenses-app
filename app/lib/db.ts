910ce6c21001b9155ec88197b31c04a4bc9cc286
pratap-panabaka <pratap.panabaka@gmail.com>
2025-11-14 10:29:13 +0530

    update for railway deploy

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3";
 import path from "path";
 
-const dbPath = path.join(process.cwd(), "database.db");
+const dbPath = path.join(process.cwd(), "app/railway/my-database.db");
 const db = new Database(dbPath);
 
 db.prepare(`
